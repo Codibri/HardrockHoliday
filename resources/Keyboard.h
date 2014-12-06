21d9183457d64234009ab10f80b1f3da07362df1
MajoDuck <majoduck@gmail.com>
2014-12-06 21:40:56 +0100

    Keyboard Test (nur Header um KOnstruktor und Destruktor erweitert)

---
+++ resources/Keyboard.h
@@ -0,0 +1,15 @@
+// Mareike
+
+#include "Engine\EngineModules.h"
+
+class Keyboard : public EngineModule {
+public:
+	Keyboard();
+
+	virtual ~Keyboard();
+
+	void update(float deltaTime, float time) override;
+
+	// TODO float getX()
+	// TODO float getY()
+};
