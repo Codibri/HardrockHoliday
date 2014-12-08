0aeccd0b2e7b6d52f8313b99a5ace18610a9c7aa
FlippinPizza <ph.osswald@gmail.com>
2014-12-08 19:24:08 +0100

    SoundManager implementiert, Singleton funktioniert noch nicht wird nachgebesert...

--- resources/headers/Audio/SoundManager.h
+++ resources/headers/Audio/SoundManager.h
@@ -1,9 +1,41 @@
+#include "root.h"
+#include "Util.h"
+
+using namespace Vektoria;
+
 #pragma once
-class SoundManager
+enum Sound
 {
-public:
-	SoundManager();
-	~SoundManager();
+	BackgroundMusic,
+	StoneRolling,
+	StoneFalling,
+	StoneBreaking
 };
 
-//test
+
+//Singleton funktioniert noch nicht, wird noch gefixt
+
+class SoundManager 
+{
+public:
+	static SoundManager* getInstance();
+	static void Init(HWND hwnd);
+	static void Fini();
+	SoundManager(HWND hwnd);
+	~SoundManager();
+    void play(Sound SoundName, bool looped=false);
+    void stop(Sound SoundName);
+	void setVolume(Sound SoundName, float volume);
+private:
+    CAudio* getSound(Sound SoundName);
+	//Je Sound (Enum) ein CAudio Object
+	//Fuer neue Sounds hier eine Private Variable anlegen und
+	//die Methode getSound(...), den Konstruktor und den Destruktor anpassen
+    CAudio mBackgroundMusic;
+    CAudio mStoneRolling;
+	CAudio mStoneFalling;
+	CAudio mStoneBreaking;
+	//static SoundManager* instance;
+};
+
+
--- resources/sources/Audio/SoundManager.cpp
+++ resources/sources/Audio/SoundManager.cpp
@@ -1,11 +1,97 @@
 #include "Audio\SoundManager.h"
+#include "Utility\DebugLog.h"
 
 
-SoundManager::SoundManager()
+
+SoundManager* SoundManager::getInstance()
 {
-}
+	/*
+	if (nullptr == instance)
+	{
+		DebugLog::getInstance()->log("Der Soundmanager wurde vor dem Aufruf nicht Initialisiert ('SoundManager::Init(HWND hwnd)')");
+		return nullptr;
+	}
+	return instance;
+	*/
+	return new SoundManager(0);
+};
 
+void SoundManager::Init(HWND hwnd)
+{
+	//instance = new SoundManager(hwnd);
+};
+
+void SoundManager::Fini()
+{
+	//instance->~SoundManager();
+};
+
+SoundManager::SoundManager(HWND hwnd)
+{
+	mBackgroundMusic.Init("GameResources\\Levels\\test.wav", hwnd);
+	mStoneRolling.Init("GameResources\\Levels\\test.wav", hwnd);
+	mStoneFalling.Init("GameResources\\Levels\\test.wav", hwnd);
+	mStoneBreaking.Init("GameResources\\Levels\\test.wav", hwnd);
+}
 
 SoundManager::~SoundManager()
 {
+	mBackgroundMusic.Fini();
+	mStoneRolling.Fini();
+	mStoneFalling.Fini();
+	mStoneBreaking.Fini();
+}
+
+void SoundManager::play(Sound SoundName, bool looped)
+{
+	CAudio* CurrentAudio = getSound(SoundName);
+	if (looped)
+		CurrentAudio->Loop();
+	else
+		CurrentAudio->Start();
+}
+
+void SoundManager::stop(Sound SoundName)
+{
+	CAudio* CurrentAudio = getSound(SoundName);
+	CurrentAudio->Stop();
+}
+
+void SoundManager::setVolume(Sound SoundName, float volume)
+{
+	CAudio* CurrentAudio = getSound(SoundName);
+	//falsche Werte abfangen
+	if (volume < 0) volume = 0;
+	if (volume > 1) volume = 1;
+	CurrentAudio->Stop();
+}
+
+CAudio* SoundManager::getSound(Sound SoundName)
+{
+	//transform Enum to SoundPointer
+	switch (SoundName)
+	{
+	case BackgroundMusic:
+	{
+		return &mBackgroundMusic;
+		break;
+	}
+	case StoneRolling:
+	{
+		return &mStoneRolling;
+		break;
+	}
+	case StoneFalling:
+	{
+		return &mStoneFalling;
+		break;
+	}
+	case StoneBreaking:
+	{
+		return &mStoneBreaking;
+		break;
+	}
+	};
+	//ab hier nur noch Fehlerbehandlung und return 0 fuer Compiler
+	return 0;
 }
